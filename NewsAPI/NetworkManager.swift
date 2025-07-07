96ae4f1f525cf4030a2e00c9634adc6657366791
metelykx <ivaschenkoodenys@gmail.com>
2025-07-07 11:57:29 +0300

    create a new varibales as url

--- NewsAPI/NetworkManager.swift
+++ NewsAPI/NetworkManager.swift
@@ -13,11 +13,17 @@ final class NetworkManager {
     // in here I created a singelton for this class
     static let shared = NetworkManager()
     
+    
+    //url for get information from API
+    let url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=56f20e2c6d354ee58b9bfc763887716a"
+    
+    
     // this is a initial for singelton
     private init() {}
     
     
     func getNews() async throws {
+        
         
     }
 }
