deefa730b0c2c773235902749432ef3d755aabc5
metelykx <ivaschenkoodenys@gmail.com>
2025-07-14 13:22:51 +0300

    new func for Text

--- NewsAPI/Extension/ExtensionDateText.swift
+++ NewsAPI/Extension/ExtensionDateText.swift
@@ -9,5 +9,17 @@ import Foundation
 import SwiftUI
 
 extension Text {
+    func titleFont() -> some View {
+        self
+            .foregroundStyle(.primary)
+            .font(.system(size: 16, weight: .bold, design: .rounded))
+                        .opacity(0.8)
+    }
     
+    func descriptionFont() -> some View {
+        self
+            .foregroundStyle(.primary)
+                        .font(.system(size: 14, weight: .regular, design: .rounded))
+                        .opacity(0.8)
+    }
 }
