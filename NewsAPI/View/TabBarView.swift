e8e2330e318f66bf416027f0a737dca8fa927c27
metelykx <ivaschenkoodenys@gmail.com>
2025-07-14 12:26:35 +0300

    new file as TabBarView

---
+++ NewsAPI/View/TabBarView.swift
@@ -0,0 +1,19 @@
+//
+//  TabBarView.swift
+//  NewsAPI
+//
+//  Created by Denis Ivaschenko on 14.07.2025.
+//
+
+import SwiftUI
+
+struct TabBarView : View {
+    var body: some View {
+        Text("ded")
+    }
+}
+
+
+#Preview {
+    TabBarView()
+}
