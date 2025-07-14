03588718722b0776912b1df3962b747aa5474349
metelykx <ivaschenkoodenys@gmail.com>
2025-07-14 12:48:41 +0300

    new file as SettingsView

---
+++ NewsAPI/View/SettingsView.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsView.swift
+//  NewsAPI
+//
+//  Created by Denis Ivaschenko on 14.07.2025.
+//
+
+import SwiftUI
+
+struct SettingsView : View {
+    var body: some View {
+        Text("dede")
+    }
+}
+
+
+#Preview {
+    SettingsView()
+}
