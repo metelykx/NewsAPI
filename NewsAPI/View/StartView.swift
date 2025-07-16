701ae588100c5749632f2c63810bbe2643cc6e6a
metelykx <ivaschenkoodenys@gmail.com>
2025-07-16 12:34:03 +0300

    new struct and add VStack

--- NewsAPI/View/StartView.swift
+++ NewsAPI/View/StartView.swift
@@ -7,3 +7,16 @@
 
 import SwiftUI
 
+struct StartView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        Text("Hefef")
+    }
+}
+
+
+#Preview {
+    StartView()
+}
