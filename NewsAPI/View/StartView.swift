2f5bd519a52f46ee0e669241b152b94ab38fe686
metelykx <ivaschenkoodenys@gmail.com>
2025-07-16 12:47:36 +0300

    add geom reader

--- NewsAPI/View/StartView.swift
+++ NewsAPI/View/StartView.swift
@@ -9,16 +9,19 @@ import SwiftUI
 
 struct StartView: View {
     var body: some View {
-        VStack {
-            Image("1", bundle: nil)
-                .resizable()
-                .scaledToFit()
+        GeometryReader { geometry in
+            
+            VStack {
+                Image("1", bundle: nil)
+                    .resizable()
+                    .scaledToFit()
                 
-            Text("NewsAPI")
-                .fontWeight(.bold)
-                .font(.title)
+                Text("NewsAPI")
+                    .fontWeight(.bold)
+                    .font(.title)
+                Spacer()
+            }
         }
-    
     }
 }
 
